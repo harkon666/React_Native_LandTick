72cfe9cd80e62408ad9e8953197648b351939307
harkon666 <bryan.wicaksanaa@gmail.com>
2020-04-01 01:41:47 +0700

    edit data ticket list for more variation

--- land_tick/component/list.component.js
+++ land_tick/component/list.component.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {Icon, List, ListItem} from '@ui-kitten/components';
-import {ListTopNav, TicketTopNav} from './topNav.component';
+import {ListTopNav} from './topNav.component';
 
-const data = new Array(8).fill({
-  title: 'Title for Item',
-  description: 'Description for Item',
-});
+const data = [
+  {
+    title: 'Jakarta - Bandung',
+    description: '15:00 - 18:00',
+    accessory: style => <Text>Rp. 150.000</Text>,
+  },
+  {
+    title: 'Bandung - Surabaya',
+    description: '15:00 - 21:00',
+    accessory: style => <Text>Rp. 175.000</Text>,
+  },
+  {
+    title: 'Jakarta - Surabaya',
+    description: '12:00 - 18:00',
+    accessory: style => <Text>Rp. 225.000</Text>,
+  },
+  {
+    title: 'Jogja - Surabaya',
+    description: '15:00 - 18:00',
+    accessory: style => <Text>Rp. 90.000</Text>,
+  },
+  {
+    title: 'Jogja - Jakarta',
+    description: '22:00 - 03:00',
+    accessory: style => <Text>Rp. 200.000</Text>,
+  },
+];
 
 export const TicketList = ({navigation}) => {
-  const renderItemAccessory = style => <Text>Rp. 320.000</Text>;
-
   const renderItemIcon = style => (
     <Icon {...style} name="navigation-2-outline" />
   );
@@ -18,10 +39,10 @@ export const TicketList = ({navigation}) => {
   const renderItem = ({item, index}) => (
     <ListItem
       key={index}
-      title={`Jakarta  - Bandung`}
-      description={`15:00 - 18:00`}
+      title={item.title}
+      description={item.description}
       icon={renderItemIcon}
-      accessory={renderItemAccessory}
+      accessory={item.accessory}
     />
   );
 
